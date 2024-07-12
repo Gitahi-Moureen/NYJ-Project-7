543d171eef48a306ef31b9124ed73881a8dde5f8
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-07-12 08:22:01 +0300

    added changes

---
+++ src/Portfolio/index.js
@@ -0,0 +1,102 @@
+import './index.css'
+const Portfolio = () => {
+    return(
+       
+        <div>
+            <h1 className="heading">Portfolio</h1>
+            <div>
+            <div className="portfolio-2">
+      <div className="image-Maggy">
+     <img src="/images/Maggy.jpeg" className='label'/>
+    <p className="skills">
+     <p>I am Moureen Gitahi.<br/> A software Developer</p>
+<b>Skills</b><br/>
+1.Expertise in HTML,CSS and Javascript for building responsive and visually appealing user interfaces.<br/>
+2.Ability to leverage popular frameworks like Django.<br/>
+3.Skilled in modern front-end frameworks and libraries such as React.js<br/>
+4.Ability to implement DOM manipulation and create dynamic web applications<br/>
+
+
+
+</p>
+     </div>
+     <div className="image-Esther">
+     <img src="/images/Esther.jpeg" className='label'/>
+     <p className="skills">
+<p>I am Moureen Gitahi.<br/> A software Developer</p>
+<b>Skills</b><br/>
+1.Expertise in HTML,CSS and Javascript for building responsive and visually appealing user interfaces.<br/>
+2.Ability to leverage popular frameworks like Django.<br/>
+3.Skilled in modern front-end frameworks and libraries such as React.js<br/>
+4.Ability to implement DOM manipulation and create dynamic web applications<br/>
+
+
+
+</p>
+     </div>
+     <div className="image-Terry">
+     <img src="/images/Terry.jpeg" className='label'/>
+     
+     <p className="skills">
+     <p>I am Moureen Gitahi.<br/> A software Developer</p>
+<b>Skills</b><br/>
+1.Expertise in HTML,CSS and Javascript for building responsive and visually appealing user interfaces.<br/>
+2.Ability to leverage popular frameworks like Django.<br/>
+3.Skilled in modern front-end frameworks and libraries such as React.js<br/>
+4.Ability to implement DOM manipulation and create dynamic web applications<br/>
+
+
+</p>
+     </div>
+     </div>
+         <div className="portfolio">
+    <div className="image-Gitahi">
+     <img src="/images/Gitahi.jpeg" className='label'/>
+     {/* <img src="/images/kitabu.webp" className="book"/> */}
+     <p className="skills">
+     <p>I am Moureen Gitahi.<br/> A software Developer</p>
+<b>Skills</b><br/>
+1.Expertise in HTML,CSS and Javascript for building responsive and visually appealing user interfaces.<br/>
+2.Ability to leverage popular frameworks like Django.<br/>
+3.Skilled in modern front-end frameworks and libraries such as React.js<br/>
+4.Ability to implement DOM manipulation and create dynamic web applications<br/>
+
+
+</p>
+     </div>
+     <div className="image-techie">
+     <img src="/images/techie.jpeg" className='label'/>
+     {/* <img src="/images/kitabu.webp" className="book"/> */}
+   <p className="skills">
+   <b>Services</b><br/>
+    We as a comminity of Software developers we give services according to our skills</p>
+ 
+     </div>
+     <div className="image-Tuukuo">
+     <img src="/images/Tuukuo.jpeg" className='label'/>
+     
+     <p className="skills">
+     <p>I am Moureen Gitahi.<br/> A software Developer</p>
+<b>Skills</b><br/>
+1.Expertise in HTML,CSS and Javascript for building responsive and visually appealing user interfaces.<br/>
+2.Ability to leverage popular frameworks like Django.<br/>
+3.Skilled in modern front-end frameworks and libraries such as React.js<br/>
+4.Ability to implement DOM manipulation and create dynamic web applications<br/>
+
+
+
+</p>
+     </div>
+     </div>
+   </div>
+   </div>
+   
+     
+    
+    
+    
+    )
+
+
+}
+export default Portfolio;
