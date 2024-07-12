f8117f0c0bc431c3b94f1843b14745a1703201dc
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-07-12 07:30:08 +0300

    created the videos section

---
+++ multimedia/src/Videos/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './index.css'
+import ReactPlayer from 'react-player';
+
+
+const Videos = ()=>{
+    return(
+
+    
+        
+                <div className="vid"> 
+                <h1>chatGPT Season 1</h1>
+                  <div className="vidLine1">
+                <div><iframe src="https://www.loom.com/embed/b47031eddc2147a99aebaf32b006af92?sid=d5b32b52-3f4c-4d29-9f41-a2951c7327bc" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 1:</b>The development of ChatGPT</p>
+                
+</div>               
+                <div ><iframe src="https://www.loom.com/embed/a456880b8c98406db4da1612a303dcd2?sid=bea2c28f-5b66-4b68-988c-3e7f4a586e5b" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 2:</b> Features of chatGPT and<br></br> how the product generates revenue.
+
+</p>
+                   
+</div>
+                <div><iframe src="https://www.loom.com/embed/8299ce6892aa4416886aea8ab1467720?sid=6ad43d45-2767-44c5-8034-0c8dd936cc82" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 3: </b>Positives and shortcomings <br></br>of chatGPT and the User experience.
+</p>
+                
+</div>
+
+</div>
+
+<div className="vidLine2">
+                <div><iframe src="https://www.loom.com/embed/785e76518a494715a08761c75c05fb8f?sid=8a19a939-8a75-479f-9b82-36a43b386aac" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 4:</b> The use case and impact of chatGPT</p>
+               
+</div>
+                <div><iframe src="https://www.loom.com/embed/c9a0867f680148cb96476650bb0948ff?sid=584fca13-1928-43cb-9466-1bd22077e4a9" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
+                <p className="text"><b>Episode 5:</b> Architecture outline of chatGPT</p>
+                <div className="likeandshare">
+                    <div><img src="/Images/blueshare.png" alt="Share" className="icon"/></div>
+                    <div><img src="/Images/like.jpeg" alt="Share" className="icon"/></div>
+                    </div> 
+</div>
+                   
+                </div>
+
+                </div>
+
+
+                
+    )
+
+};
+
+export default Videos;
